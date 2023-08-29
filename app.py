b3c2d72a297314d54121e5fec665a1105c65da0c
Aryan <singlaaryan1902@gmail.com>
2023-08-29 23:39:54 +0530

    text and emojis

--- app.py
+++ app.py
@@ -7,26 +7,35 @@ def roll_dice():
 
 def get_fortune(num):
     fortunes = {
-        1: "IT IS CERTAIN",
+        1: "IT IS CERTAIN ",
         2: "DON'T COUNT ON IT ",
-        3: "YES, DEFINITELY",
-        4: "SIGNS POINT TO YES",
-        5: "CANNOT PREDICT NOW",
-        6: "MY SOURCES SAY NO"
+        3: "YES, DEFINITELY ",
+        4: "SIGNS POINT TO YES ",
+        5: "CANNOT PREDICT NOW ",
+        6: "MY SOURCES SAY NO "
     }
     
     return fortunes.get(num)
 
 def main():
-    st.title('FORTUNE WHISPER')
-    st.header('SUMMON YOUR CURIOSITIES !!!')
+    st.title('FORTUNE WHISPER :speak_no_evil:')
+    st.text('SUMMON YOUR CURIOSITIES !!! ')
     current_year = datetime.datetime.now().year
     if st.button('ASK THE UNIVERSE :8ball:'):
         result = roll_dice()
         fortune = get_fortune(result)
         if fortune:
             st.text(fortune)
-    st.markdown('<div style="height: 400px;"></div>', unsafe_allow_html=True)
+        emojis = [
+            ':face_exhaling:', 
+            ':no_mouth:', 
+            ':raised_hands:', 
+            ':grimacing:', 
+            ':face_with_spiral_eyes:', 
+            ':disappointed_relieved:'
+        ]
+        st.markdown(emojis[result - 1])                                     
+    st.markdown('<div style="height: 250px;"></div>', unsafe_allow_html=True)
             
     st.markdown(
         f'<footer style="position: absolute; bottom: 0; width: 100%; text-align: center;">&copy; {current_year} Aryan Singla</footer>',
@@ -34,3 +43,5 @@ def main():
     )
 if __name__ == '__main__':
     main()
+
+
