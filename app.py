66d56f0bbf15eb44f136c1e16a87727e0dc9e847
Aryan <singlaaryan1902@gmail.com>
2023-08-29 23:03:00 +0530

    update footer

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random
-
+import datetime
 def roll_dice():
     num = random.randint(1, 6)
     return num
@@ -20,11 +20,17 @@ def get_fortune(num):
 def main():
     st.title('FORTUNE WHISPER')
     st.text('SPIN THE BALL:')
+    current_year = datetime.datetime.now().year
     if st.button(':8ball:'):
         result = roll_dice()
         fortune = get_fortune(result)
         if fortune:
             st.text(fortune)
-
+    st.markdown('<div style="height: 400px;"></div>', unsafe_allow_html=True)
+            
+    st.markdown(
+        f'<footer style="position: absolute; bottom: 0; width: 100%; text-align: center;">&copy; {current_year} Aryan Singla</footer>',
+        unsafe_allow_html=True
+    )
 if __name__ == '__main__':
     main()
